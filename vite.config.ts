ca879a274fcbf06812d4c83daa0d34aaeb3c0772
matt <matthewcheung1003@gmail.com>
2025-01-11 22:36:06 +0800

    update gh-pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/daily-calendar-puzzle-solver/",
   plugins: [react()],
 })
